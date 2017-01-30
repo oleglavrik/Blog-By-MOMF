b543cd5c64ef7b3e00d02dad21645f36a9dd34fc
oleh lavryk <oleh.lavrik@gmail.com>
2017-01-30 17:30:35 +0200

    Leave only basic files

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,16 +1,19 @@
 <?php
-
+/**
+ * Test class for example:
+ */
 namespace app\models;
 
 use vendor\core\Models\BaseModel;
 
 class User extends BaseModel
 {
-    public function getUsers(){
+    public function getUsers()
+    {
         $dbh = $this->db->prepare("SELECT * FROM user");
         $dbh->execute();
 
-        if($dbh->rowCount()){
+        if ($dbh->rowCount()) {
             return $dbh->fetchAll(\PDO::FETCH_ASSOC);
         }
     }
